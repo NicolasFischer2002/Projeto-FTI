6f72dbcea4a9582e34e5870a10a25e58151bf1db
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2022-12-27 22:35:33 -0300

    Add files via upload

--- U_Principal.pas
+++ U_Principal.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
-  Vcl.StdCtrls;
+  Vcl.StdCtrls, Vcl.Buttons;
 
 type
   TF_Principal = class(TForm)
@@ -16,11 +16,15 @@ type
     Pnl_Center: TPanel;
     Pnl_TopPurple: TPanel;
     TImage_LogoCenter: TImage;
-    TImage_Dashboard: TImage;
-    TImage_Investimentos: TImage;
-    TImage_Crud: TImage;
-    procedure FormResize(Sender: TObject);
+    Pnl_LeftTop: TPanel;
+    SB_Atualizar: TSpeedButton;
+    SB_Investimentos: TSpeedButton;
+    SB_Dashboard: TSpeedButton;
+    TImage_Home: TImage;
+    Label1: TLabel;
+    Pnl_TopHome: TPanel;
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormResize(Sender: TObject);
   private
     { Private declarations }
   public
@@ -48,103 +52,17 @@ end;
 
 procedure TF_Principal.FormResize(Sender: TObject);
 begin
-     try
-        if IsZoomed(F_Principal.Handle) then
-         begin
-              Pnl_LeftPai.Width := 400;
+     if F_Principal.WindowState = WsMaximized then
+      begin
+           Pnl_LeftPai.Width := 400;
 
-              // Logo central
+           Pnl_LeftTop.Height := 200;
 
-              TImage_LogoLeftTop.Picture.LoadFromFile('Img/Logo/LogoSuperiorEsquerdaEscuraMaior.png');
-              TImage_LogoLeftTop.Width  := 315;
-              TImage_LogoLeftTop.Height := 105;
+           TImage_LogoLeftTop.Width  := 315;
+           TImage_LogoLeftTop.Height := 100;
 
-              // Responsividade logo Center
-
-              TImage_LogoCenter.Left := trunc((F_Principal.Width - TImage_LogoCenter.Width) / 2.82);
-              TImage_LogoCenter.Top  := trunc((F_Principal.Height - TImage_LogoCenter.Height) / 4);
-
-              // Dashboard
-              TImage_Dashboard.Picture.LoadFromFile('Img/Icones/Dashboard.png');
-              TImage_Dashboard.Width  := 64;
-              TImage_Dashboard.Height := 64;
-
-              TImage_Dashboard.Left := trunc((F_Principal.Width - TImage_Dashboard.Width) / 25);
-              TImage_Dashboard.Top  := trunc((F_Principal.Height - TImage_Dashboard.Height) / 4);
-
-
-              // Investimentos
-              TImage_Investimentos.Picture.LoadFromFile('Img/Icones/Investimentos.png');
-              TImage_Investimentos.Width  := 64;
-              TImage_Investimentos.Height := 64;
-
-              TImage_Investimentos.Left := trunc((F_Principal.Width - TImage_Investimentos.Width) / 25);
-              TImage_Investimentos.Top  := trunc((F_Principal.Height - TImage_Investimentos.Height) / 2);
-
-
-              // Crud
-
-              TImage_Crud.Picture.LoadFromFile('Img/Icones/Crud.png');
-              TImage_Crud.Width  := 64;
-              TImage_Crud.Height := 64;
-
-              TImage_Crud.Left := trunc((F_Principal.Width - TImage_Crud.Width) / 25);
-              TImage_Crud.Top  := trunc((F_Principal.Height - TImage_Crud.Height) / 1.30);
-
-         end
-        else
-         begin
-              Pnl_LeftPai.Width := 300;
-
-              TImage_LogoLeftTop.Picture.LoadFromFile('Img/Logo/LogoSuperiorEsquerdaEscuraMenor.png');
-              TImage_LogoLeftTop.Width  := 229;
-              TImage_LogoLeftTop.Height := 80;
-
-              // Responsividade logo Center
-
-              TImage_LogoCenter.Left := trunc((F_Principal.Width - TImage_LogoCenter.Width) / 3.5);
-              TImage_LogoCenter.Top  := trunc((F_Principal.Height - TImage_LogoCenter.Height) / 4);
-
-              // Dashboard
-              TImage_Dashboard.Picture.LoadFromFile('Img/Icones/Dashboard.png');
-              TImage_Dashboard.Width  := 64;
-              TImage_Dashboard.Height := 64;
-
-              TImage_Dashboard.Left := trunc((F_Principal.Width - TImage_Dashboard.Width) / 25);
-              TImage_Dashboard.Top  := trunc((F_Principal.Height - TImage_Dashboard.Height) / 4);
-
-
-              // Investimentos
-              TImage_Investimentos.Picture.LoadFromFile('Img/Icones/Investimentos.png');
-              TImage_Investimentos.Width  := 64;
-              TImage_Investimentos.Height := 64;
-
-              TImage_Investimentos.Left := trunc((F_Principal.Width - TImage_Investimentos.Width) / 25);
-              TImage_Investimentos.Top  := trunc((F_Principal.Height - TImage_Investimentos.Height) / 2);
-
-
-              // Crud
-
-              TImage_Crud.Picture.LoadFromFile('Img/Icones/Crud.png');
-              TImage_Crud.Width  := 64;
-              TImage_Crud.Height := 64;
-
-              TImage_Crud.Left := trunc((F_Principal.Width - TImage_Crud.Width) / 25);
-              TImage_Crud.Top  := trunc((F_Principal.Height - TImage_Crud.Height) / 1.30);
-         end;
-
-     Except
-//     on E: Exception do
-//        begin
-//             Application.MessageBox('Cuidado! A responsividade pode travar sua máquina!',
-//                                     'Confirmação',mb_YesNo+mb_IconInformation);
-//             Application.Terminate;
-//        end;
-
-          Application.Terminate; // Impede que o Delphi trave a máquina tentando dividir por 0
-
-     end;
-
+           TImage_LogoLeftTop.Picture.LoadFromFile('Img/Logo/LogoSuperiorEsquerdaEscuraMaior.png');
+      end;
 end;
 
 end.
