7c5aedc62fefc5389e9ac7385957c5828153c2cb
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2023-01-12 21:42:09 -0300

    Add files via upload

--- U_Investimentos.pas
+++ U_Investimentos.pas
@@ -4,11 +4,27 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Buttons, Vcl.Imaging.pngimage;
 
 type
   TF_Investimentos = class(TForm)
+    Pnl_Right: TPanel;
+    Edt_Ativo: TEdit;
+    Lbl_Ativo: TLabel;
+    Edt_Quantidade: TEdit;
+    Lbl_Quantidade: TLabel;
+    Edt_Lucro: TEdit;
+    Lbl_Lucro: TLabel;
+    Edt_Taxas: TEdit;
+    Lbl_Taxas: TLabel;
+    Pnl_Center: TPanel;
+    Pnl_Top: TPanel;
+    Pnl_Bottom: TPanel;
     StringGrid1: TStringGrid;
+    Pnl_Manager: TPanel;
+    TImage_Manager: TImage;
+    Lbl_Manager: TLabel;
   private
     { Private declarations }
   public
